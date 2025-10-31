94d0d28ea6ce6f6c007bad124d5c7a10217f0a92
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-31 19:41:19 +0300

    Update ls.py

--- src/ls.py
+++ src/ls.py
@@ -6,34 +6,35 @@ from logging import getLogger
 
 logger = getLogger(__name__)
 
+
 def ls_function(user_input: list) -> None:
     """
         Функция для обработки команды ls
     """
-    l_mode = False # Флаг подробного вывода информации (-l)
-    path = '' # Путь к директории для просмотра
+    l_mode = False  # Флаг подробного вывода информации (-l)
+    path = ''  # Путь к директории для просмотра
     """ Парсинг аргументов команды """
     for i in user_input:
-        if i == 'ls' and user_input.count('ls') == 1: # Пропускает название команды
+        if i == 'ls' and user_input.count('ls') == 1:  # Пропускает название команды
             continue
-        elif i == '-l' and l_mode == False: # Устанавливает режим подробного вывода
+        elif i == '-l' and l_mode == False:  # Устанавливает режим подробного вывода
             l_mode = True
-        elif len(path) == 0: # Запоминает путь, если он указан
+        elif len(path) == 0:  # Запоминает путь, если он указан
             path = i
-        else: # Обрабатывает случай лишних аргументов
+        else:  # Обрабатывает случай лишних аргументов
             logger.error(f'ls: unrecognized option "{' '.join(user_input[1:])}"')
             raise ValueError(f'ls: unrecognized option "{' '.join(user_input[1:])}"')
-    if os.path.isdir(path) or len(path) == 0: # Проверка существования директории
+    if os.path.isdir(path) or len(path) == 0:  # Проверка существования директории
         if path != '':
-            files = os.listdir(path) # Получает список файлов по указанному пути
+            files = os.listdir(path)  # Получает список файлов по указанному пути
         else:
-            files = os.listdir() # Получает список файлов текущей директории
-        if files and l_mode == False: # Простой вывод списка файлов
+            files = os.listdir()  # Получает список файлов текущей директории
+        if files and l_mode == False:  # Простой вывод списка файлов
             for i in range(len(files)):
                 print(files[i])
             print('_____')
             logger.info(f"{' '.join(user_input)}")
-        else: # Подробный вывод информации о файлах
+        else:  # Подробный вывод информации о файлах
             for i in range(len(files)):
                 if len(path) != 0:
                     file_info = os.stat(path + '/' + files[i])
@@ -44,6 +45,6 @@ def ls_function(user_input: list) -> None:
                       datetime.fromtimestamp(file_info.st_mtime), files[i])
             print('_____')
             logger.info(f"{' '.join(user_input)}")
-    else: # Обрабатывает случай несуществующей директории
+    else:  # Обрабатывает случай несуществующей директории
         logger.error('no such directory')
         raise FileNotFoundError('no such directory')
