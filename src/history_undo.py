ab96379265076d3625a1b16ee7e17fcd8e468899
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-31 19:44:52 +0300

    Create history_undo.py

---
+++ src/history_undo.py
@@ -0,0 +1,59 @@
+import logging
+import os
+from cp import cp_function
+from mv import mv_function
+from rm import rm_function
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+history = []
+cp_mv_rm = []
+trash_history = []
+
+
+def append_history(s, k: [list, int]) -> None:
+    history.append(f'{k}. {' '.join(s)}')
+    with open('.history', 'a', encoding='utf-8') as file:
+        file.write(f'{' '.join(s)}\n')
+
+
+def history_function(s: list) -> None:
+    if s[1]:
+        try:
+            s[1] = int(s[1])
+            if s[1] > len(history):
+                for i in s:
+                    print(i)
+            elif s[1] >= 0:
+                for i in range(-s[1], 0):
+                    print(history[i])
+            logger.info(f"{' '.join(s)}")
+        except Exception:
+            logger.error(f'history: unrecognized option "{' '.join(s[1:])}"')
+            raise ValueError(f'history: unrecognized option "{' '.join(s[1:])}"')
+    elif len(s) == 1:
+        logger.info(f"{' '.join(s)}")
+        for i in s:
+            print(i)
+    else:
+        logger.error(f'history: unrecognized option "{' '.join(s[1:])}"')
+        raise ValueError(f'history: unrecognized option "{' '.join(s[1:])}"')
+
+
+def undo_function(last: list) -> None:
+    if last[0] == 'cp':
+        rm_function(['rm', f'{last[1]}'])
+        logger.info("undo")
+    elif last[0] == 'mv':
+        mv_function([last[0], last[1], last[2]])
+        logger.info("undo")
+    elif last[0] == 'rm':
+        cp_function(['rm', os.path.join('.trash', f'{trash_history[-1]}'), os.getcwd()])
+        trash_history.pop(-1)
+        logger.info("undo")
+    else:
+        logger.error(f'undo: unrecognized option "{' '.join(last)}"')
+        raise ValueError(f'undo: unrecognized option "{' '.join(last)}"')
+    history.pop(-1)
+    cp_mv_rm.pop(-1)
