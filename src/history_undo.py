dc223c230264caffff00ee2a54586f80aaac364e
Катя <eagalanova@mai.education>
2025-11-14 19:39:17 +0300

    Initial commit

--- src/history_undo.py
+++ src/history_undo.py
@@ -44,42 +44,52 @@ def history_function(s: list) -> None:
 
 def undo_function(last: list) -> None:
     if last[0] == 'cp':
-        path1 = ''
-        path2 = ''
-        path3 = ''
-        for i in last:
-            if i != 'cp' and i != '-r':
-                if len(path1) == 0:
-                    path1 = i
-                elif len(path2) == 0:
-                    path2 = i
-                elif len(path3) == 0:
-                    path3 = i
-        cd_function(['cd', path2])
-        rm_function(['rm', path1])
-        cd_function(['cd', path3])
-        logger.info("undo for cp")
+        try:
+            path1 = ''
+            path2 = ''
+            path3 = ''
+            for i in last:
+                if i != 'cp' and i != '-r':
+                    if len(path1) == 0:
+                        path1 = i
+                    elif len(path2) == 0:
+                        path2 = i
+                    elif len(path3) == 0:
+                        path3 = i
+            cd_function(['cd', path2])
+            rm_function(['rm', path1])
+            cd_function(['cd', path3])
+            logger.info("undo for cp")
+        except Exception:
+            print(f'не удалось отменить команду:{' '.join(last)}')
     elif last[0] == 'mv':
-        path1 = ''
-        path2 = ''
-        path3 = ''
-        for i in last:
-            if i != 'mv':
-                if len(path1) == 0:
-                    path1 = i
-                elif len(path2) == 0:
-                    path2 = i
-                elif len(path3) == 0:
-                    path3 = i
-        cd_function(['cd', path2])
-        mv_function(['mv', path1, path3])
-        cd_function(['cd', path3])
-        logger.info("undo for mv")
+        try:
+            path1 = ''
+            path2 = ''
+            path3 = ''
+            for i in last:
+                if i != 'mv':
+                    if len(path1) == 0:
+                        path1 = i
+                    elif len(path2) == 0:
+                        path2 = i
+                    elif len(path3) == 0:
+                        path3 = i
+            cd_function(['cd', path2])
+            mv_function(['mv', path1, path3])
+            cd_function(['cd', path3])
+            logger.info("undo for mv")
+        except Exception:
+            print(f'не удалось отменить команду:{' '.join(last)}')
     elif last[0] == 'rm':
-        cp_function(['cp', os.path.join('.trash', f'{trash_history[-1]}'), last[-1]])
-        trash_history.pop(-1)
-        logger.info("undo for rm")
+        try:
+            cp_function(['cp', os.path.join('.trash', f'{trash_history[-1]}'), last[-1]])
+            trash_history.pop(-1)
+            logger.info("undo for rm")
+        except Exception:
+            print(f'не удалось отменить команду:{' '.join(last)}')
     else:
         logger.error(f'undo: unrecognized option')
         raise ValueError(f'undo: unrecognized option')
-    cp_mv_rm.pop(-1)
+    if len(cp_mv_rm)>0:
+        cp_mv_rm.pop(-1)
